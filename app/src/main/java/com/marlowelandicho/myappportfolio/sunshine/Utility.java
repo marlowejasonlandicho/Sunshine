eb8251f5552784bc485f6dcc15889510d3e81e22
Marlowe Landicho <marlowe.landicho@sungard.com>
2015-06-20 17:50:00 +0800

    Lesson 4C

---
+++ app/src/main/java/com/marlowelandicho/myappportfolio/sunshine/Utility.java
@@ -0,0 +1,7 @@
+package com.marlowelandicho.myappportfolio.sunshine;
+
+/**
+ * Created by marlowe.landicho on 20/6/15.
+ */
+public class Utility {
+}
---
+++ app/src/main/java/com/marlowelandicho/myappportfolio/sunshine/data/WeatherProvider.java
@@ -0,0 +1,7 @@
+package com.marlowelandicho.myappportfolio.sunshine.data;
+
+/**
+ * Created by marlowe.landicho on 20/6/15.
+ */
+public class WeatherProvider {
+}
